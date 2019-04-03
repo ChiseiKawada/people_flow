c535ddc55f9af5a40e4003d16c8aba6d798bcadc
Chisei Kawada <ckawada@mxdlab.net>
2019-04-03 17:46:30 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+ Using SQLAlchemy and Flask get db record.(GET)
+"""
+
+from flask import *  # 必要なライブラリのインポート
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, func
+from flaski.database import db_session
+import os
+import sqlite3
+import datetime
+#from flask import Flask, render_template, abort
+from flaski.models import People_flow
+
+app = Flask(__name__)
+#app.config['DEBUG'] = True
+
+# 起動されたサーバーの/にアクセスした時の挙動を記述。
+# @app.route("/hoge")で記述すれば、http://127.0.0.1:5000/aaでアクセスした時の挙動を記述できる。
+@app.route("/")
+def main():
+    contents = People_flow.query.all()
+    # index.htmlにcontensを引数に渡して実行。
+    return render_template("index.html", contents=contents)
+
+#sigfoxから受信したデータをDBに格納する
+@app.route("/people_count", methods=["GET", "POST"])
+def people():
+
+    people_data = []
+    all_column = People_flow.query.all()
+
+    if request.method == "POST":
+        people_data  = []
+        people_data  = request.json
+
+        column_first = People_flow.query.first()
+        people_flow = People_flow()
+        people_flow.count = float(people_data["people_float"])
+        people_flow.date = datetime.datetime.now()
+
+        db_session.add(people_flow)
+        db_session.commit()
+
+    return render_template("people.html", info=people_data, all_column=all_column)
+
+@app.route("/getInfo", methods=["GET", "POST"])
+def get_info():
+    pass
+
+if __name__ == "__main__":  # 実行されたら
+    app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)  # デバッグモード、localhost:8888 で スレッドオンで実行
---
+++ flaski/models.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from sqlalchemy import Column, Integer, String, Text, DateTime, Float
+from flaski.database import Base
+from datetime import datetime
+
+class People_flow(Base):
+    __tablename__ = 'people_flow'                  # テーブル名
+    count = Column(Float, primary_key=True)                           # カラム１(count) 計測した人数
+    date = Column(DateTime, default=datetime.now()) # カラム２(date) デフォルト現在日時を設定
+
+    def __init__(self, count=None, date=None):
+        self.count = count
+        self.date = date
+
+    def __repr__(self):
+        return '<Title %r>' % (self.title)
+
+# class WikiContent(Base):
+#    __tablename__ = 'wikicontents'                  # テーブル名
+#    id = Column(Integer, primary_key=True)          # カラム１(id)
+#    title = Column(String(128), unique=True)        # カラム２(title)
+#    body = Column(Text)                             # カラム3(body)
+#    date = Column(DateTime, default=datetime.now()) # カラム４(date) デフォルト現在日時を設定
+
+#    def __init__(self, title=None, body=None, date=None):
+#        self.title = title
+#        self.body = body
+#        self.date = date
+
+#    def __repr__(self):
+#        return '<Title %r>' % (self.title)
